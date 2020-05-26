e05ee318bdef95f59970f4446305f3ec8dc19c27
Steven Yee <s.yee615@gmail.com>
2020-05-26 11:43:33 -0400

    Added dynamic programming section

---
+++ dynamic_programming.js
@@ -0,0 +1,13 @@
+
+// ############################################################################################################
+
+// Best time to buy and sell stock
+var maxProfit = function(prices) {
+    var min = prices[0]; 
+    var max = 0;
+    for (var i = 0; i < prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        max = Math.max(max, prices[i] - min);
+    }
+    return max;
+};
