904912cf7e175c60b50999bb5a93fd788a7314b9
Steven Yee <s.yee615@gmail.com>
2020-06-09 16:56:48 -0400

    Going back to array problems

--- arrays.js
+++ arrays.js
@@ -254,3 +254,31 @@ var generate = function(numRows) {
     
     return pascal;
 };
+
+
+// ############################################################################################################
+
+//Sort Colors
+
+var sortColors = function(nums) {
+    if (nums.length === 0 || nums.length === 1) return;
+    
+    let start = 0;
+    let end = nums.length - 1;
+    let index = 0;
+    
+    while (index <= end && start < end) {
+        if (nums[index] === 0){
+            nums[index] = nums[start];
+            nums[start] = 0;
+            start++;
+            index++;
+        } else if (nums[index] === 2){
+            nums[index] = nums[end];
+            nums[end] = 2;
+            end--;
+        } else {
+            index++;
+        }
+    }
+};
