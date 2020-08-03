3467ca725345501ee1070f4976060195519dce66
Steven Yee <s.yee615@gmail.com>
2020-08-03 15:25:11 -0400

    Back to trees

---
+++ Trees/102.binary_tree_level_order_traversal.js
@@ -0,0 +1,27 @@
+const levelOrder = (root) => {
+    if (!root) return [];
+    
+    let result = [];
+    let queue = [root];
+    
+    while(queue.length){
+        let size = queue.length;
+        let currentLevel = [];
+        for(let i = 0; i < size; i++){
+            let current = queue.shift();
+            currentLevel.push(current.val);
+            
+            if (current.left !== null){
+                queue.push(current.left);
+            }
+            
+            if (current.right !== null){
+                queue.push(current.right);
+            }
+        }
+        
+        result.push(currentLevel);
+    }
+    
+    return result;
+}
