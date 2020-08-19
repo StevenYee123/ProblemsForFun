313fd3d157a6a42972cde5fbce6185a93a64832e
Steven Yee <s.yee615@gmail.com>
2020-08-19 12:32:01 -0400

    Intersection of two linked lists

---
+++ LinkedLists/160.intersection_of_two_linked_lists.js
@@ -0,0 +1,29 @@
+var getIntersectionNode = function(headA, headB) {
+    if (headA === null || headB === null){
+        return null;
+    }
+    
+    let pointer1 = headA;
+    let pointer2 = headB;
+    
+    while (pointer1 !== pointer2){
+        pointer1 = pointer1.next;
+        pointer2 = pointer2.next;
+        
+        //This handles the case that there is no intersection i.e. both are null...
+        if (pointer1 === pointer2){
+            return pointer1;
+        }
+        
+        if (pointer1 === null){
+            pointer1 = headB;
+        }
+        
+        if (pointer2 === null){
+            pointer2 = headA;
+        }
+    }
+    
+    //Either pointer would work
+    return pointer1;
+};
