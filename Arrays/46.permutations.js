92174afc8d9912df1b9c75453f30f161ef78b23a
Steven Yee <s.yee615@gmail.com>
2020-08-24 13:15:24 -0400

    Permutations

---
+++ Arrays/46.permutations.js
@@ -0,0 +1,13 @@
+var permute = function(nums, set = [], answers = []) {
+    if (!nums.length) answers.push([...set]);
+    
+    for(let i = 0; i < nums.length; i++){
+        const newNums = nums.filter((n, index) => index !== i);
+        set.push(nums[i]);
+        permute(newNums, set, answers);
+        //Very important to pop! Prevents the same index from being reused 
+        set.pop();
+    }
+    
+    return answers;
+};
