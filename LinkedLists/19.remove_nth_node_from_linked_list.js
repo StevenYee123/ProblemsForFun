d58b89a2396a6af52790d3fe8d86fd702101d3ff
Steven Yee <s.yee615@gmail.com>
2020-09-04 14:51:45 -0400

    Comments for problem

--- LinkedLists/19.remove_nth_node_from_linked_list.js
+++ LinkedLists/19.remove_nth_node_from_linked_list.js
@@ -8,12 +8,12 @@ var removeNthFromEnd = function(head, n) {
     for(let i = 0; i < n; i++){
         fast = fast.next;
     }
-    
+    //We reach the actual end of the list
     while(fast && fast.next){
         slow = slow.next;
         fast = fast.next;
     }
-    
+    //Delete a node by changing pointers around
     slow.next = slow.next.next;
     
     return dummy.next;
