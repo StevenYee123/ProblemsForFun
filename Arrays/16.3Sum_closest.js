25267428f1cfda992f7a53820533fb0d3ed214b5
Steven Yee <s.yee615@gmail.com>
2020-09-04 14:41:38 -0400

    3 Sum Closest

---
+++ Arrays/16.3Sum_closest.js
@@ -0,0 +1,21 @@
+var threeSumClosest = function(nums, target) {
+    //Sorting plays a huge role!!
+    nums.sort((a, b) => a - b);
+    
+    let closest = Infinity;
+    for(let i = 0; i < nums.length - 2; i++){
+        let left = i + 1;
+        let right = nums.length - 1;
+        while(left < right){
+            let sum = nums[i] + nums[left] + nums[right];
+            if(Math.abs(sum - target) < Math.abs(closest - target)) closest = sum;
+            if(sum > target){
+                right--;
+            } else {
+                left++;
+            }
+        }
+    }
+    
+    return closest;
+};
