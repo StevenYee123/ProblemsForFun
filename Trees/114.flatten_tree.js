ad86871a6d5aad4f24a8a4793b414bae606de951
Steven Yee <s.yee615@gmail.com>
2020-09-16 17:41:35 -0400

    flatten

---
+++ Trees/114.flatten_tree.js
@@ -0,0 +1,24 @@
+const flatten = (root) => {
+    const list = [];
+    helper(root, list);
+    
+    if(list.length === 0 ) return;
+    
+    for(let i = 0; i < list.length - 1; i++) {
+        const node = list[i];
+        node.left = null;
+        node.right = list[i + 1];
+    }
+    
+    const lastNode = list[list.length -1];
+    lastNode.left = null;
+    lastNode.right = null;
+};
+
+const helper = (node, list) => {
+    if(!node) return;
+    
+    list.push(node);
+    helper(node.left, list);
+    helper(node.right, list);
+}
