49f42d452113698c7e3355105a63e0c2f042bc9a
Steven Yee <s.yee615@gmail.com>
2020-09-21 15:47:15 -0400

    Longest palindrome

---
+++ Strings/409.longest_palindrome.js
@@ -0,0 +1,14 @@
+var longestPalindrome = function(s) {
+  let ans = 0;
+  let keys = {};
+  
+    
+  for (let char of s) {
+    keys[char] = (keys[char] || 0) + 1;
+      //Just add as we go!
+    if (keys[char] % 2 == 0) ans += 2;
+  }
+    
+    //If the length is greater than our answer we can add one extra!
+  return s.length > ans ? ans + 1 : ans;
+};
