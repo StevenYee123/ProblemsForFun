97dec1a246fc13c3f97b3aa2c7bb0cd5b9366b93
Steven Yee <s.yee615@gmail.com>
2020-09-29 22:23:53 -0400

    Frequency Sort

---
+++ Arrays/451.frequency_sort.js
@@ -0,0 +1,21 @@
+var frequencySort = function(s) {
+    let charCount = {};
+    for(let i = 0; i < s.length; i++){
+        let currChar = s[i];
+        if(charCount[currChar] !== undefined){
+            charCount[currChar]++;
+        } else {
+            charCount[currChar] = 1;
+        }
+    }
+    
+    let sorted = Object.keys(charCount).sort((a, b) => charCount[b] - charCount[a]);
+    let res = "";
+    
+    for(let i = 0; i < sorted.length; i++){
+        let curr = sorted[i];
+        res += curr.repeat(charCount[curr]);
+    }
+    
+    return res;
+};
