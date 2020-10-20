1bc2118aaba7f2aba2a37fc011dc1adabb3160bb
Steven Yee <s.yee615@gmail.com>
2020-10-19 21:14:44 -0400

    Product of array except self

---
+++ Dynamic Programming/238.product_of_array_except_self.js
@@ -0,0 +1,18 @@
+var productExceptSelf = function(nums) {
+    let output = [];
+    
+    let leftProduct = 1;
+    let rightProduct = 1;
+    
+    for(let i = 0; i < nums.length; i++){
+        output[i] = leftProduct;
+        leftProduct *= nums[i];
+    }
+    
+    for(let j = nums.length - 1; j >= 0; j--){
+        output[j] *= rightProduct;
+        rightProduct *= nums[j];
+    }
+    
+    return output;
+};
