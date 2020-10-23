95a9b50ee389400731b5f48c67ac5e56911304ba
Steven Yee <s.yee615@gmail.com>
2020-10-22 21:01:16 -0400

    Linked lists

--- LinkedLists/19.remove_nth_node_from_linked_list.js
+++ LinkedLists/19.remove_nth_node_from_linked_list.js
@@ -2,7 +2,7 @@ var removeNthFromEnd = function(head, n) {
     let dummy = new ListNode(0);
     dummy.next = head;
     let slow = dummy;
-    let fast = dummy;
+    let fast = dummy;  
     
     //Here we start a fast pointer to index away from the end
     for(let i = 0; i < n; i++){
