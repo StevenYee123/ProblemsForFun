d5c9ff4686ead63ee1ada544fe888a3997fe353d
Steven Yee <s.yee615@gmail.com>
2020-10-26 16:56:19 -0400

    Swap node in pairs

--- LinkedLists/24.swap_nodes_in_pairs.js
+++ LinkedLists/24.swap_nodes_in_pairs.js
@@ -4,11 +4,17 @@ var swapPairs = function(head) {
     let current = temp;
     
     while(current.next && current.next.next){
+        // Establish the two base points
         let firstNode = current.next;
         let secondNode = current.next.next;
+        //Set up our "next"
         firstNode.next = secondNode.next;
+
+        //Appropriately assign the 2 next nodes
         current.next = secondNode;
         current.next.next = firstNode;
+
+        //Make sure we traverse
         current = current.next.next;
     }
     
