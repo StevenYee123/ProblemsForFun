36186285f740917edd3bc462f31757bebf30c6a2
Steven Yee <s.yee615@gmail.com>
2020-10-28 14:53:59 -0400

    Find first and last position of target in sorted array

---
+++ Arrays/34.find_first_and_last_position_of_target.js
@@ -0,0 +1,26 @@
+var searchRange = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while(left <= right){
+        let mid = Math.floor((left + right)/2);
+        if(nums[mid] === target){
+            left = mid;
+            right = mid;
+            break;
+        }
+        
+        if(nums[mid] > target){
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    
+    if(left > right) return [-1, -1];
+    
+    while(nums[left] === target) left--;
+    while(nums[right] === target) right++;
+    
+    return [left + 1, right - 1];
+};
