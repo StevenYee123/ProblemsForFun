c3d3a9ecec7f989f0e9fe74d8237ee35e58609cb
Steven Yee <s.yee615@gmail.com>
2020-11-23 18:35:29 -0500

    Find first and last position of target in sorted array

--- Arrays/34.find_first_and_last_position_of_target.js
+++ Arrays/34.find_first_and_last_position_of_target.js
@@ -1,9 +1,11 @@
 var searchRange = function(nums, target) {
+    if(!nums.length) return [-1, -1];
+    
     let left = 0;
     let right = nums.length - 1;
     
-    while(left <= right){
-        let mid = Math.floor((left + right)/2);  
+    while(left < right){
+        let mid = Math.floor((left + right)/2);
         if(nums[mid] === target){
             left = mid;
             right = mid;
@@ -11,16 +13,41 @@ var searchRange = function(nums, target) {
         }
         
         if(nums[mid] > target){
-            right = mid - 1;
+            right = mid;
         } else {
             left = mid + 1;
         }
     }
     
-    if(left > right) return [-1, -1];
-    
     while(nums[left] === target) left--;
     while(nums[right] === target) right++;
     
-    return [left + 1, right - 1];
+    return left === right ? [-1, -1] : [left + 1, right - 1];
 };
+
+// var searchRange = function(nums, target) {
+//     let left = 0;
+//     let right = nums.length - 1;
+    
+//     while(left <= right){
+//         let mid = Math.floor((left + right)/2);  
+//         if(nums[mid] === target){
+//             left = mid;
+//             right = mid;
+//             break;
+//         }
+        
+//         if(nums[mid] > target){
+//             right = mid - 1;
+//         } else {
+//             left = mid + 1;
+//         }
+//     }
+    
+//     if(left > right) return [-1, -1];
+    
+//     while(nums[left] === target) left--;
+//     while(nums[right] === target) right++;
+    
+//     return [left + 1, right - 1];
+// };
