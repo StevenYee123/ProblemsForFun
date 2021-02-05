13cc61d1088969d5fe6f9b603786316ce809f489
Steven Yee <s.yee615@gmail.com>
2021-02-04 21:36:08 -0500

    Implemented trie insert and print

--- Trees/00.Tries.js
+++ Trees/00.Tries.js
@@ -10,11 +10,29 @@ class Trie{
         this.root = new Node();
     }
 
-    insert(word){
+    insert(word, root = this.root){
+        let letter = word[0];
+        if(!(letter in root.children)){
+            root.children[letter] = new Node();
+        }
 
+        //Meaning the end of the word has been reached, create a terminal node
+        if(word.length === 1){
+            root.children[letter].isTerminal = true;
+        } else {
+            this.insert(word.slice(1), root.children[letter]);
+        }
+    }
+
+    print(root = this.root){
+        for(let letter in root.children){
+            console.log(letter);
+            this.print(root.children[letter]);
+        }
     }
 }
 
 let myTrie = new Trie();
 myTrie.insert("ten");
 myTrie.insert("tea");
+myTrie.print();
